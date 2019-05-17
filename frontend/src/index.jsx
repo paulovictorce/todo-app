d3a26e8209cc7ba365dd183ddcf364686b4cd3e4
Paulo Victor <paulovictorce@hotmail.com>
2019-05-17 18:14:00 -0300

    Implementando as dependências do redux e react-redux e criação do primeiro reducer.

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,5 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+//import para usar o redux
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
 import App from './main/app'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+//Impotação do Reducer
+import reducers from './main/reducers'
+
+//criando a store(estado controlado pelo redux)
+const store = createStore(reducers)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('app'))
