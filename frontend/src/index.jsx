b8cacb23f0b448c9d64cb285f79a0d333c92608f
Paulo Victor <paulovictorce@hotmail.com>
2019-05-18 09:43:23 -0300

    Continuando a implementação do redux. Criação das actions add e search e implementação do middleware que auxilia a exibição da lista de tarefas

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,16 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 //import para usar o redux
-import { createStore } from 'redux'
+import { applyMiddleware, createStore } from 'redux'
 import { Provider } from 'react-redux'
+import promise from 'redux-promise'
 
 import App from './main/app'
 
 //Impotação do Reducer
 import reducers from './main/reducers'
 
+//A linha abaixo sincroniza a aplicação com o plugin redux devtools dd Chrome
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
 //criando a store(estado controlado pelo redux)
-const store = createStore(reducers)
+const store = applyMiddleware(promise)(createStore)(reducers, devTools)
 
 ReactDOM.render(
     <Provider store={store}>
